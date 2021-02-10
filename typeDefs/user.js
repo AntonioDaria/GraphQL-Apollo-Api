a9a62da2a42764a867facb44122cf21c22acdbd0
Antonio D'aria <antoniodaria@msn.com>
2021-02-10 10:42:17 +0000

    test commit

--- typeDefs/user.js
+++ typeDefs/user.js
@@ -1,6 +1,6 @@
 const { gql } = require("apollo-server-express");
 
- //use gql from apollo server to define the Schema
+//use gql from apollo server to define the Schema
 module.exports = gql`
   extend type Query {
     users: [User!]
